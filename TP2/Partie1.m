1e73bedd8c507cb26225d6b068013615e0f7ff13
Guillaume Arruda <guillaume.arruda@gmail.com>
2016-02-13 15:03:45 -0500

    validation test apprentissage

--- TP2/Partie1.m
+++ TP2/Partie1.m
@@ -15,14 +15,42 @@ taux_dapprentissage = 0.0005;
 possibleY = eye(n);
 converged = false;
 yixi = Y' * X';
-lastLogVraisemblance = 0;
-
+lastLogVraisemblance = -realmax;
+[XA XV XT YA YV YT] = create_train_valid_test_splits(X,Y);
+i = 0;
 while ~converged
     logVraisemblance = sum(sum(((Y * Theta) .* X')') - log(sum(exp(possibleY * Theta * X))));
-    converged = abs(lastLogVraisemblance - logVraisemblance) < 0.1;
+    converged = logVraisemblance - lastLogVraisemblance  < 0.1;
     lastLogVraisemblance = logVraisemblance;
-    Z = repmat(sum(exp(possibleY * Theta * X)),4,1);
-    esperance = ((exp(possibleY * Theta * X)./Z)' * possibleY)' * X';
+    %Apprentissage
+    Z = repmat(sum(exp(possibleY * Theta * XA)),4,1);
+    esperance = ((exp(possibleY * Theta * XA)./Z)' * possibleY)' * XA';
     gradient = yixi - esperance;
     Theta = Theta + (taux_dapprentissage * gradient);
+    
+    %Validation
+    Z = repmat(sum(exp(possibleY * Theta * XV)),4,1);
+    reponse = exp(possibleY * Theta * XV)./Z;
+    [reponse, index] = max(reponse);
+    result = ones(size(reponse,2),4);
+    result = result * 2;
+    for i = 1 : size(index,2)
+        result(i, index(i)) = 1;
+    end
+    d = result == YV;
+    precision = sum(sum((result == YV))) / size(result,1)
+    i = i+1;
 end
+
+%Test
+Z = repmat(sum(exp(possibleY * Theta * XV)),4,1);
+reponse = exp(possibleY * Theta * XV)./Z;
+[reponse, index] = max(reponse);
+result = ones(size(reponse,2),4);
+result = result * 2;
+for i = 1 : size(index,2)
+    result(i, index(i)) = 1;
+end
+d = result == YV;
+precision = sum(sum((result == YV))) / size(result,1)
+i = i+1;
