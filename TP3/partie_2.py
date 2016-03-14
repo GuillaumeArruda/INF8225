5f75c96dd21e76b2d26f13a936241db4e81fa2f5
Guillaume Arruda <guillaume.arruda@gmail.com>
2016-03-13 23:40:21 -0400

    Tp3 fonctionnel

--- TP3/partie_2.py
+++ TP3/partie_2.py
@@ -1,5 +1,6 @@
 ﻿import gzip
 import pickle
+import random
 import numpy as np
 
 def numberListToOneHotVectors(values, size):
@@ -15,19 +16,19 @@ def pw_linear(x, a):
         return a * x
 
 def pw_linear_derivative(x,a):
-    if(x > 0):
+    if(x >= 0):
         return 1
     else:
         return a
    
 #Softmax function found at https://gist.github.com/aelaguiz/4453854
 def softmax(w):
-    w = np.array(w)
+    w = np.array(w.T)
     maxes = np.amax(w, axis=1)
     maxes = maxes.reshape(maxes.shape[0], 1)
     e = np.exp(w - maxes)
     dist = e / np.sum(e, axis=1, keepdims=True)
-    return dist
+    return dist.T
 
 vector_pw_linear = np.vectorize(pw_linear, otypes=[np.float])
 vector_pw_linear_derivative = np.vectorize(pw_linear_derivative, otypes=[np.float])
@@ -56,8 +57,6 @@ class NeuralLayer:
     def backpropagate(self, X, Y):
         if self.nextLayer == None:
             self.delta = -(Y - self.activation)
-            #lossFunction = np.sum(Y * np.log(self.activation))
-            #print lossFunction
         else:
             self.nextLayer.backpropagate(X,Y)
             derivative = vector_pw_linear_derivative(self.inValue, self.a)
@@ -68,31 +67,37 @@ class NeuralLayer:
             self.nextLayer.update(minibatchSize)
         self.theta = self.theta - self.learning_rate * np.dot(self.delta, self.input.T)
 
+    def lossfunction(self, Y):
+        if(self.nextLayer == None):
+            return -np.sum(Y * np.log(self.activation)) / len(Y)
+        else:
+            return self.nextLayer.lossfunction(Y)
+
 class NeuralNetwork:
     def __init__(self, layers, a, learning_rate):
         self.starting_layer = NeuralLayer(None,layers, len(layers) - 1, a, learning_rate)
 
-    def train(self, train_x, train_y, valid_x, valid_y, minibatchSize):
+    def train(self, train_x, train_y, valid_x, valid_y,valid_y_oh, minibatchSize):
         n = len(train_x)
-        mini_batches_x = [train_x[k:k+minibatchSize] for k in xrange(0, n, minibatchSize)]
-        mini_batches_y = [train_y[k:k+minibatchSize] for k in xrange(0, n, minibatchSize)]
         converged = False
         lastPrecision = -1
         precision = 0
-        while ~converged:
-            i = 0
+        lastLoss = float("inf")
+        loss = 0
+        while not converged:
+            random.shuffle(zip(train_x,train_y))
+            mini_batches_x = [train_x[k:k+minibatchSize] for k in xrange(0, n, minibatchSize)]
+            mini_batches_y = [train_y[k:k+minibatchSize] for k in xrange(0, n, minibatchSize)]
             for mini_batch_x, mini_batch_y in zip(mini_batches_x, mini_batches_y):
                 f = self.starting_layer.feedfoward(mini_batch_x.T)
                 f = self.starting_layer.backpropagate(mini_batch_x.T, mini_batch_y.T)
                 self.starting_layer.update(minibatchSize)
-                i = i + 1
-                if i % 10 == 0:
-                    precision = self.evaluate(valid_x.T, valid_y.T)
-                    converged = precision < lastPrecision
-                    lastPrecision = precision
-                    print precision
-                    if converged:
-                        break
+            precision = self.evaluate(valid_x.T, valid_y.T)
+            loss = self.starting_layer.lossfunction(valid_y_oh.T)
+            converged = loss > lastLoss
+            lastLoss = loss
+            print precision
+            print loss
 
     def evaluate(self, test_x, test_y):
         results = self.starting_layer.feedfoward(test_x)
@@ -105,17 +110,17 @@ if __name__ == '__main__':
         train_set,valid_set, test_set = pickle.load(f)
     np.random.seed(1)
     number_of_possible_y = 10
-    learning_rate = 0.00005
+    learning_rate = 0.0005
     train_x, train_y = train_set
     valid_x, valid_y = valid_set
     test_x, test_y = test_set
     train_y_oh = numberListToOneHotVectors(train_y, number_of_possible_y)
     valid_y_oh = numberListToOneHotVectors(valid_y, number_of_possible_y)
-    test_y_oh = numberListToOneHotVectors(test_y, number_of_possible_y)
 
-    layers = np.array([784, 30, 10])
+    layers = np.array([784, 300, 100, 10])
     network = NeuralNetwork(layers, 0.05 , learning_rate)
-    network.train(train_x, train_y_oh, valid_x, valid_y, 100)
+    network.train(train_x, train_y_oh, valid_x, valid_y,valid_y_oh, 100)
+    print network.evaluate(test_x.T,test_y.T)
 
 
 
