94f99a90d476d35747f5a10ff6f126eb10950e51
Guillaume Arruda <guillaume.arruda@gmail.com>
2016-03-14 19:38:03 -0400

    TP3 fini

--- TP3/partie_2.py
+++ TP3/partie_2.py
@@ -2,7 +2,7 @@
 import pickle
 import random
 import numpy as np
-
+import csv
 def numberListToOneHotVectors(values, size):
     oneHotVectors = np.zeros([values.size, size])
     for (i,value) in enumerate(values):
@@ -10,18 +10,23 @@ def numberListToOneHotVectors(values, size):
     return oneHotVectors
 
 def pw_linear(x, a):
-    if(x >= 0):
-        return x
-    else:
-        return a * x
+    mask = x.copy()
+    mask[mask >= 0] = 1.
+    mask[mask < 0] = a
+    return mask * x
 
 def pw_linear_derivative(x,a):
-    if(x >= 0):
-        return 1
-    else:
-        return a
-   
-#Softmax function found at https://gist.github.com/aelaguiz/4453854
+    mask = x.copy()
+    mask[mask >= 0] = 1.
+    mask[mask < 0] = a
+    return mask
+
+csvfile = open('courbe_2_layer.csv','wb')
+csvwriter = csv.writer(csvfile)
+alpha = 0.6
+
+
+#stable softmax function found at https://gist.github.com/aelaguiz/4453854
 def softmax(w):
     w = np.array(w.T)
     maxes = np.amax(w, axis=1)
@@ -29,9 +34,6 @@ def softmax(w):
     e = np.exp(w - maxes)
     dist = e / np.sum(e, axis=1, keepdims=True)
     return dist.T
-
-vector_pw_linear = np.vectorize(pw_linear, otypes=[np.float])
-vector_pw_linear_derivative = np.vectorize(pw_linear_derivative, otypes=[np.float])
 
 class NeuralLayer:
     def __init__(self,previous_layer, layers, number_of_layers, a, learning_rate):
@@ -43,6 +45,7 @@ class NeuralLayer:
         self.theta = (np.random.randn(layers[-number_of_layers],layers[-number_of_layers - 1] + 1)) / np.sqrt(layers[-number_of_layers - 1] + 1)
         self.a = a
         self.learning_rate = learning_rate
+        self.nabla_theta = np.zeros(self.theta.shape)
 
     def feedfoward(self, X):
         self.input = np.append(X,np.ones((1,np.size(X,axis=1))),axis=0)
@@ -51,7 +54,7 @@ class NeuralLayer:
             self.activation = softmax(self.inValue)
             return self.activation
         else:
-            self.activation = vector_pw_linear(self.inValue, self.a)
+            self.activation = pw_linear(self.inValue, self.a)
             return self.nextLayer.feedfoward(self.activation)
 
     def backpropagate(self, X, Y):
@@ -59,13 +62,14 @@ class NeuralLayer:
             self.delta = -(Y - self.activation)
         else:
             self.nextLayer.backpropagate(X,Y)
-            derivative = vector_pw_linear_derivative(self.inValue, self.a)
+            derivative = pw_linear_derivative(self.inValue, self.a)
             self.delta = derivative * np.dot(self.nextLayer.theta[:,:-1].T, self.nextLayer.delta)
 
     def update(self, minibatchSize):
         if self.nextLayer != None:
             self.nextLayer.update(minibatchSize)
-        self.theta = self.theta - self.learning_rate * np.dot(self.delta, self.input.T)
+        self.nabla_theta = alpha * self.nabla_theta + np.dot(self.delta, self.input.T)
+        self.theta = self.theta - self.learning_rate * self.nabla_theta
 
     def lossfunction(self, Y):
         if(self.nextLayer == None):
@@ -84,6 +88,8 @@ class NeuralNetwork:
         precision = 0
         lastLoss = float("inf")
         loss = 0
+        i = 0
+        j = 0
         while not converged:
             random.shuffle(zip(train_x,train_y))
             mini_batches_x = [train_x[k:k+minibatchSize] for k in xrange(0, n, minibatchSize)]
@@ -92,12 +98,17 @@ class NeuralNetwork:
                 f = self.starting_layer.feedfoward(mini_batch_x.T)
                 f = self.starting_layer.backpropagate(mini_batch_x.T, mini_batch_y.T)
                 self.starting_layer.update(minibatchSize)
+                if j % 10 == 0:
+                    precision = self.evaluate(valid_x.T, valid_y.T)
+                    loss = self.starting_layer.lossfunction(valid_y_oh.T)
+                    csvwriter.writerow([j, loss, precision])
+                j = j + 1
             precision = self.evaluate(valid_x.T, valid_y.T)
             loss = self.starting_layer.lossfunction(valid_y_oh.T)
             converged = loss > lastLoss
             lastLoss = loss
-            print precision
-            print loss
+            #csvwriter.writerow([i, loss, precision])
+            i = i + 1
 
     def evaluate(self, test_x, test_y):
         results = self.starting_layer.feedfoward(test_x)
@@ -105,10 +116,11 @@ class NeuralNetwork:
         correct = sum(int(x == y) for (x,y) in zip(results, test_y))
         return float(correct) / np.size(test_x,axis=1)
 
+
+
 if __name__ == '__main__':
     with gzip.open('mnist.pkl.gz','rb') as f:
         train_set,valid_set, test_set = pickle.load(f)
-    np.random.seed(1)
     number_of_possible_y = 10
     learning_rate = 0.0005
     train_x, train_y = train_set
@@ -116,11 +128,13 @@ if __name__ == '__main__':
     test_x, test_y = test_set
     train_y_oh = numberListToOneHotVectors(train_y, number_of_possible_y)
     valid_y_oh = numberListToOneHotVectors(valid_y, number_of_possible_y)
-
-    layers = np.array([784, 300, 100, 10])
+    test_y_oh = numberListToOneHotVectors(test_y,number_of_possible_y)
+    layers = np.array([784, 10])
     network = NeuralNetwork(layers, 0.05 , learning_rate)
     network.train(train_x, train_y_oh, valid_x, valid_y,valid_y_oh, 100)
     print network.evaluate(test_x.T,test_y.T)
+    print network.starting_layer.lossfunction(test_y_oh.T)
+    raw_input()
 
 
 
